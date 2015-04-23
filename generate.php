65b0163d7c457d2006a35b355f0bb7d4e6433f94
Darren Wong <darren@inkling.com>
2015-04-22 19:34:35 -0700

    10 Bands
    Update entire repo with that new new

---
+++ generate.php
@@ -0,0 +1,6 @@
+<?php
+	// require('LoremIpsum.class.php');
+	// $generator = new LoremIpsumGenerator;
+	// $paragraphs = 54;
+	echo $_POST["numOfP"];
+?>
--- index.php
+++ index.php
@@ -3,15 +3,14 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-    <title>
-      Drake Ipsum
-    </title>
+    <title>Drake Ipsum</title>
     <meta name="author" content="Russell Shearer & Darren Wong">
     <meta name="description" content="If you’re reading this set your type with Drake.">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/drake.css">
     <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   </head>
   <body>
     <img class="ovo-logo" src="img/ovo.svg" alt="OVO unruly">
@@ -20,28 +19,31 @@
     <section class="bg-cloud">
       <small>N<sup>o</sup> of Paragraphs</small>
       <div class="ipsum-controls">
-      <input class="ipsum" type="tel" name="ipsum_count" value="6" maxlength="2">
-      <input class="ipsum-generate" type="submit" name="ipsum_generate" value="">
+        <?php 
+          if(isset($_POST['submit'])){ //check if form was submitted
+            $input = $_POST['numOfP']; //get input text
+            $paragraphs = ($input * 10) - $input;
+          }    
+        ?>
+        <form action="" method="post">
+          <input name="numOfP" id="ipsum_input" class="ipsum" type="tel" name="paragraph_count" maxlength="2" value="">
+          <input name="submit" class="ipsum-generate" type="submit" name="Generate" value="">
+          <button name="copy" class="bg-gold button" type="submit" name="copy" value="Copy Text">Copy Text</button>
+        </form>   
+      </div>
+      <div class="ipsum-text">
+        <?php 
+          require('LoremIpsum.class.php');
+          $generator = new LoremIpsumGenerator; 
+          echo $generator->getContent($paragraphs, 'html', false);
+        ?>
       </div>
     </section>
-
-    <?php
-
-    require('LoremIpsum.class.php');
-    $generator = new LoremIpsumGenerator;
-    //100 words in html format
-    $generator->getContent(100);
-
-    ?>
-    <div class="bg-gold button">Copy Text</div>
-    <div class="ipsum-text">
-    <p>My woes this is that nasty flow way up, I feel blessed.Got a lot of enemies oh my God I'mma write my wrongs.Uh, thinkin' out loud Smart kids that smoke weed, Honor Roll. You only want what's real you just never found it I remember when my schedule was as flexible as she is.Don't talk to me like I'm famous, and don't assume cause I don't respect assumptions babe. For all the stuntin' I'll forever be immortalized I apologize it happened over time.</p>
-    </div>
     <footer>
       <div class="bg-white button">Share with your friends</div>
       <small>No new friends tho.</small>
       <hr>
-      <small>This was created real quick by <a href="">@russted</a> & <a href="">@darrendub</a><small>
+      <small>This was created <em>real</em> quick by <a target="_blank" href="http://russted.com">russted</a> & <a href="">@darrendub</a> using the magics of <a href="http://tinsology.net/scripts/php-lorem-ipsum-generator/">Tinsology</a><small>
       <small>Set in <a href="">Range</a> and hosted on the Github.</small>
     </footer>
   </body>
